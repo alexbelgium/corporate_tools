72128f86db46bc7ed6151d3a3897c9394b78ddd8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-04-17 16:38:57 +0200

    Update touch_files.ps1

--- touch_files.ps1
+++ touch_files.ps1
@@ -1,5 +1,13 @@
-# Define the path to the OneDrive folder
-$folderPath = 'C:\path\to\your\onedrive\folder'
+# Get the script's current directory as the default folder path
+$defaultFolderPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# Ask the user to confirm the default folder path or enter a new one
+$folderPath = Read-Host "Press Enter to use the default folder path ($defaultFolderPath) or enter a new folder path"
+
+# If the user does not input anything, use the default folder path
+if ([string]::IsNullOrWhiteSpace($folderPath)) {
+    $folderPath = $defaultFolderPath
+}
 
 # Get the current date and time
 $currentDate = Get-Date
